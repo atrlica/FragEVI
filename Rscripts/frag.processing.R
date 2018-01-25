09ea4551a4e78c32e70c95ec35af482b21aecb1d
Andrew Trlica <atrlica@bu.edu>
2018-01-24 23:54:49 -0500

    Initial work to develop 1m NDVI from NAIP CIR imagery.

--- Rscripts/frag.processing.R
+++ Rscripts/frag.processing.R
@@ -3,10 +3,10 @@ library(sp)
 library(rgdal)
 library(rgeos)
 library(raster)
-# library(data.table)
+library(data.table)
 # library(ff)
 
-setwd("/projectnb/buultra/atrlica/FragEVI/")
+# setwd("/projectnb/buultra/atrlica/FragEVI/")
 ### load EVI composite for grid and process for ISA grid
 evi.r <- raster("processed/EVI/030005-6_2010-2012_EVI.tif")
 # evi.r <- projectRaster(from=evi.r, crs=master.crs, res = 30, method = "ngb")
@@ -39,57 +39,18 @@ evi.isa.cr <- crop(evi.isa, extent(isa.na.agg))
 writeRaster(evi.isa.cr, filename="processed/evi.isa.tif", format="GTiff", overwrite=T)
 
 frag.stack <- stack(evi.isa.cr, isa.na.agg)
-# # evi.AOI <- crop(evi.r, extent(AOI))
-# # writeRaster(evi.AOI, filename="processed/EVI/030005-6_2010-2012_EVI_NAD83_AOI.tif", format="GTiff", overwrite=T)
-# # evi.AOI <- raster("processed/EVI/030005-6_2010-2012_EVI_NAD83_AOI.tif")
-# 
-# # isa.na.agg <- crop(isa.na.agg, extent(evi.isa.agg))
-# evi.isa <- crop(evi.isa,extent(isa.na.agg))
-# 
-# frag.stack <- stack(evi.isa, isa.na.agg)
-# 
-# AOI.isa <- spTransform(AOI, crs(isa.na.agg))
-# isa.na.agg <- crop(isa.na.agg, extent(AOI.isa))
-# isa.na.agg <- extend(isa.na.agg, extent(evi.AOI.isa))
-# evi.AOI.isa <- crop(evi.AOI.isa, extent(AOI.isa))
-# frag.stack <- stack(evi.AOI.isa, isa.na.agg)
-# 
-# isa.AOI <- projectRaster(from=isa.na.agg, crs=master.crs, res=30, method="ngb", filename="processed/isa_30mgrid.tif", format="GTiff", overwrite=T)
-# 
-# 
-# ### read in AOI for crs description
+
+### read in AOI for crs description
 # AOI <- readOGR(dsn="/projectnb/buultra/atrlica/BosAlbedo/data/AOI/AOI_simple_NAD83UTM19N/", layer="AOI_simple_NAD83UTM19N")
 # master.crs <- crs(AOI)
-# 
-# 
-# ### add EVI, ISA, and LULC to a stack
-# 
-# 
-# plot(isa.AOI)
-# plot(AOI, add=T)
-# plot(evi.AOI)
-# plot(AOI, add=T)
-# # fun <- function(x){
-# #   x[x==16] <- NA
-# #   return(x)
-# # }
-# # isa.na <- calc(isa, fun)
-# strip <- 0:15
-# tmp0 <- getValues(isa, row=seq(from=1, to=16), nrow=16)
-# tmp0[tmp0==16] <- NA
-# tmp0.r <- raster(matrix(data = tmp0, ncol=ncol(isa)))
-# for(s in 1:15){
-#   a <- ((strip[s]*16)+1)
-#   b <- ((strip[s]+1)*16)
-#   tmp <- getValues(isa, row=seq(from=a, to=b), nrow=16)
-#   tmp[tmp==16] <- NA
-#   assign(paste("tmp", s, sep=""), value = raster(matrix(data = tmp, ncol=ncol(isa))))
-#   
-#   isa <- setValues(isa, )
-#   print(paste("strip", s, sep=" "))
-# }
-# isa.dat <- as.big.matrix(isa)
-# isa.dat <- ff(vmode="double", dim=c(ncell(isa), 1), filename = "processed/isa.ffdata")
-# isa.dat[isa.dat==16, isa.dat:=NA]
-# 
-# 
+
+
+### Process NAIP 1m data to NDVI
+### NOTE SO MUCH: BAND ORDER IN NAIP CIR DATA IS RED GREEN BLUE NIR (1,2,3,4)
+naip.test <- stack("Z:/Ultra2/Users/atrlica/FragEVI/NAIP/EssexCIR/m_4207009_ne_19_h_20160706/m_4207009_ne_19_h_20160706.tif")
+naip.dat <- as.data.table(as.data.frame(naip.test))
+names(naip.dat) <- c("band1", "band2", "band3", "band4")
+naip.dat[, ndvi:=((band4-band1)/(band4+band1))]
+ndvi.r <- raster(naip.test[[1]])
+ndvi.r <- setValues(x = ndvi.r, values=naip.dat[,ndvi])
+writeRaster(ndvi.r, filename="processed/NAIP.ndvi.test.tif", format="GTiff", overwrite=T)
