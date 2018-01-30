0f950b664e5a5ec715150de1e49df0e8656a3b44
Andrew Trlica <atrlica@bu.edu>
2018-01-30 16:49:12 -0500

    Cleaned up frag.processing. Current capability: 1) Aggregate ISA to 30 m by EVI grid; 2) Get 1m Canopy + 2.4m NDVI into common grid, produce veg cover map for barren/grass/tree
    To do: Integrate python scripts that handle the NDVI resampling, and do the canopy edge class identification.
    
    Adding beginnings of python script to handle canopy edge ID

--- Rscripts/frag.processing.R
+++ Rscripts/frag.processing.R
@@ -4,45 +4,49 @@ library(rgdal)
 library(rgeos)
 library(raster)
 library(data.table)
-# library(ff)
+library(ff)
 
 # setwd("/projectnb/buultra/atrlica/FragEVI/")
+
+### read in AOI for crs description
+# AOI <- readOGR(dsn="/projectnb/buultra/atrlica/BosAlbedo/data/AOI/AOI_simple_NAD83UTM19N/", layer="AOI_simple_NAD83UTM19N")
+# master.crs <- crs(AOI)
+
 ### load EVI composite for grid and process for ISA grid
-evi.r <- raster("processed/EVI/030005-6_2010-2012_EVI.tif")
+evi.r <- raster("processed/EVI/030005-6_2010-2012_EVI.tif") ## this is the July 2010-2012 AOI EVI composite
+
 # evi.r <- projectRaster(from=evi.r, crs=master.crs, res = 30, method = "ngb")
 # writeRaster(evi.r, filename="processed/EVI/030005-6_2010-2012_EVI_NAD83.tif", format="GTiff", overwrite=T)
 
-
-## extent object is c(xmin, xmax, ymin, ymax)
-
-### get ISA fraction per grid (~Urban intensity?)
-### don't fart around with the ISA grid until it's stacked with a 30 m EVI and cropped -- 
-### too hard to monkey with, leave in its native state until the end
+####
+### process ISA fraction per 30 m EVI gridcell (~Urban intensity?)
+### don't fart around with the ISA grid until it's stacked with a 30 m EVI and cropped -- too hard to monkey with, leave in its native state until the end
 
 # isa <- raster("/projectnb/buultra/atrlica/BosAlbedo/data/ISA/isa_1m_AOI.tif")
-# # isa.dat <- ff(vmode="double", filename="processed/isa.raw.ff")
-# # isa.dat <- as.vector(isa)
-# # isa.dat[isa.dat==16] <- NA
-# # isa <- setValues(isa, isa.dat)
+
+### the data-only approach doesn't seem to work -- file is too large to read in, possibly see big.memory
+# isa.dat <- ff(vmode="double", filename="processed/isa.raw.ff")
+# isa.dat <- as.vector(isa)
+# isa.dat[isa.dat==16] <- NA
+# isa <- setValues(isa, isa.dat)
+
+### raster-native approach -- takes a long time
+## correct the 16 values to NA to get a proper aggregated mean value per cell (0 = not impervious, 1 = impervious)
 # fun <- function(x){
 #   x[x==16] <- NA
 #   return(x)
 # }
 # isa.na <- calc(isa, fun)
-# 
-# ### aggregate to 30m 
 # isa.na.agg <- aggregate(isa.na, fact=30, fun=mean, na.rm=FALSE) # get mean ISA per 30 m footprint
 # writeRaster(isa.na.agg, filename="processed/isa.30m.tif", format="GTiff", overwrite=T)
+
+### read in the 30 m ISA, align to the EVI grid and stack
 isa.na.agg <- raster("processed/isa.30m.tif")
-evi.isa <- projectRaster(from=evi.r, crs=crs(isa.na.agg), res=30, method="bilinear")
-evi.isa.cr <- crop(evi.isa, extent(isa.na.agg))
-writeRaster(evi.isa.cr, filename="processed/evi.isa.tif", format="GTiff", overwrite=T)
-
+# evi.isa <- projectRaster(from=evi.r, crs=crs(isa.na.agg), res=30, method="bilinear")
+# evi.isa.cr <- crop(evi.isa, extent(isa.na.agg))
+# writeRaster(evi.isa.cr, filename="processed/evi.isa.tif", format="GTiff", overwrite=T)
+evi.isa.cr <- raster("processed/evi.isa.tif")
 frag.stack <- stack(evi.isa.cr, isa.na.agg)
-
-### read in AOI for crs description
-# AOI <- readOGR(dsn="/projectnb/buultra/atrlica/BosAlbedo/data/AOI/AOI_simple_NAD83UTM19N/", layer="AOI_simple_NAD83UTM19N")
-# master.crs <- crs(AOI)
 
 
 ### Boston -- combine high-res NDVI and canopy map
