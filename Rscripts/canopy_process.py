7ed910b8254a3bb5352af319cb4391e24eaf39ff
Andrew Trlica <atrlica@bu.edu>
2018-02-02 14:23:17 -0500

    Cleaned up fragmentation process R script. Attempting to integrate canopy buffer python script. Adding canopy_process.py -- appears to be working or nearly working, uses raster-based Expand to find edge fractions

---
+++ Rscripts/canopy_process.py
@@ -0,0 +1,43 @@
+# import system modules 
+import arcpy, arcinfo
+from arcpy import env
+arcpy.CheckOutExtension("Spatial")
+
+# Set environment settings
+arcpy.env.workspace = "C:/Users/atrlica/Documents/ArcGIS/Default.gdb/"
+arcpy.env.overwriteOutput = True
+
+dbpath = "C:/Users/atrlica/Documents/ArcGIS/Default.gdb/"
+
+# convert 1m 0/1 canopy raster to poly
+canRastIn = "E:/FragEVI/data/dataverse_files/bostoncanopy_1m.tif"
+arcpy.RasterToPolygon_conversion(in_raster=canRastIn, out_polygon_features=dbpath + "bos_can_P", simplify="NO_SIMPLIFY", raster_field="VALUE")
+print("converted canopy raster to polygon")
+
+# select polygons that are 0 canopy
+arcpy.Select_analysis(in_features=dbpath + "bos_can_P", out_feature_class=dbpath+"bos_can_0", where_clause="gridcode = 0")
+print("isolated canopy gaps")
+
+# select gaps that are larger than gap size threshold
+gapSizeThresh = 50
+arcpy.Select_analysis(in_features=dbpath+"bos_can_0", out_feature_class=dbpath+"bos_can_0_filt", where_clause="Shape_Area >= "+str(gapSizeThresh))
+print("removed canopy gaps below " + str(gapSizeThresh) + " m2 in area")
+
+# convert filtered gap polygon to Raster
+arcpy.PolygonToRaster_conversion(in_features=dbpath+"bos_can_0_filt", value_field="gridcode", out_rasterdataset=dbpath+"bos_can_0_R", cell_assignment="CELL_CENTER", priority_field="gridcode", cellsize="1")
+print("converted filtered gap polys to raster")
+
+# Create expand map of 10, 20, 30 m buffers around gaps
+buffs = [10, 20, 30]
+rasterDump = "F:/FragEVI/processed/"
+for b in range(0,2):
+    try:
+        arcpy.gp.Expand_sa(dbpath+"bos_can_0_R", dbpath+"nocan_"+str(buffs[b])+"mbuff", str(buffs[b]), "0")
+        arcpy.RasterToOtherFormat(dbpath+"nocan_"+str(buffs[b])+"mbuff", rasterDump+"nocan_"+str(buffs[b])+"mbuff", "TIFF")
+        print("did " + str(buffs[b]) + "m buffer and wrote raster .tif to /processed")
+    except Exception as e:
+        print(e)
+
+
+
+
--- Rscripts/frag.processing.R
+++ Rscripts/frag.processing.R
@@ -4,34 +4,35 @@ library(rgdal)
 library(rgeos)
 library(raster)
 library(data.table)
-library(ff)
+library(rPython)
+# library(ff)
 
-# setwd("/projectnb/buultra/atrlica/FragEVI/")
+
+#######
+### the following chunk aggregates 1m ISA to 30m landsat grid
+### intermediate steps run on the cluster
 
 ### read in AOI for crs description
 # AOI <- readOGR(dsn="/projectnb/buultra/atrlica/BosAlbedo/data/AOI/AOI_simple_NAD83UTM19N/", layer="AOI_simple_NAD83UTM19N")
 # master.crs <- crs(AOI)
 
 ### load EVI composite for grid and process for ISA grid
-evi.r <- raster("processed/EVI/030005-6_2010-2012_EVI.tif") ## this is the July 2010-2012 AOI EVI composite
-
+# evi.r <- raster("processed/EVI/030005-6_2010-2012_EVI.tif") ## this is the July 2010-2012 AOI EVI composite
 # evi.r <- projectRaster(from=evi.r, crs=master.crs, res = 30, method = "ngb")
 # writeRaster(evi.r, filename="processed/EVI/030005-6_2010-2012_EVI_NAD83.tif", format="GTiff", overwrite=T)
 
-####
 ### process ISA fraction per 30 m EVI gridcell (~Urban intensity?)
 ### don't fart around with the ISA grid until it's stacked with a 30 m EVI and cropped -- too hard to monkey with, leave in its native state until the end
-
 # isa <- raster("/projectnb/buultra/atrlica/BosAlbedo/data/ISA/isa_1m_AOI.tif")
 
-### the data-only approach doesn't seem to work -- file is too large to read in, possibly see big.memory
-# isa.dat <- ff(vmode="double", filename="processed/isa.raw.ff")
-# isa.dat <- as.vector(isa)
-# isa.dat[isa.dat==16] <- NA
-# isa <- setValues(isa, isa.dat)
+# ### the data-only approach doesn't seem to work -- file is too large to read in, possibly see big.memory
+# # isa.dat <- ff(vmode="double", filename="processed/isa.raw.ff")
+# # isa.dat <- as.vector(isa)
+# # isa.dat[isa.dat==16] <- NA
+# # isa <- setValues(isa, isa.dat)
 
 ### raster-native approach -- takes a long time
-## correct the 16 values to NA to get a proper aggregated mean value per cell (0 = not impervious, 1 = impervious)
+### correct the 16 values to NA to get a proper aggregated mean value per cell (0 = not impervious, 1 = impervious)
 # fun <- function(x){
 #   x[x==16] <- NA
 #   return(x)
@@ -41,128 +42,50 @@ evi.r <- raster("processed/EVI/030005-6_2010-2012_EVI.tif") ## this is the July 
 # writeRaster(isa.na.agg, filename="processed/isa.30m.tif", format="GTiff", overwrite=T)
 
 ### read in the 30 m ISA, align to the EVI grid and stack
-isa.na.agg <- raster("processed/isa.30m.tif")
+isa.na.agg <- raster("E:/FragEVI/processed/stack/isa.30m.tif")
 # evi.isa <- projectRaster(from=evi.r, crs=crs(isa.na.agg), res=30, method="bilinear")
 # evi.isa.cr <- crop(evi.isa, extent(isa.na.agg))
 # writeRaster(evi.isa.cr, filename="processed/evi.isa.tif", format="GTiff", overwrite=T)
-evi.isa.cr <- raster("processed/evi.isa.tif")
-frag.stack <- stack(evi.isa.cr, isa.na.agg)
+evi.r <- raster("E:/FragEVI/processed/EVI/030005-6_2010-2012_EVI_NAD83.tif") ## this is the July 2010-2012 AOI EVI composite
+isa.stack <- projectRaster(isa.na.agg, evi.r, method="bilinear")
+
+frag.stack <- stack(evi.r, isa.stack)
+names(frag.stack) <- c("evi", "isa")
+writeRaster(frag.stack, filename="E:/FragEVI/processed/EVI.ISA.30mstack.tiff", format="GTiff", overwrite=T)
 
 
-### Boston -- combine high-res NDVI and canopy map
+### Boston -- combine high-res NDVI and canopy map to produce 1m veg classification map
 
 #### important notes re. land cover class using Quickbird 2.4m NDVI w/ 1m canopy presence map
 ### not readily possible in R to align/resample the two different maps into a common grid -- severe registration error in NDVI after processing
-### used Arcmap to resample + snap to 1m grid (bilinear) for NDVI 2.4m -- get good alignment with original data and features in canopy map
+### use Arcmap to resample + snap to 1m grid (bilinear) for NDVI 2.4m -- get good alignment with original data and features in canopy map
 
-## old attempted approach in R -- results in bad alignment and crummy classification maps
-# ### read in 2.4m Quickbird NDVI map, filter for NA -- NOTE: working on the NDVI in R messes it up too badly to use
-# bos.ndvi <- raster("E:/FragEVI/data/NDVI/NDVI.img") ## original Quickbird NDVI, manually process for 0==NA
-# bos.ndvi.dat <- as.data.table(as.data.frame(bos.ndvi))
-# bos.ndvi.dat[NDVI==0, NDVI:=NA]
-# bos.ndvi.na <- setValues(bos.ndvi, values = bos.ndvi.dat[,NDVI]) ## good, full map, UTM 19N
-# writeRaster(bos.ndvi.na, filename="E:/FragEVI/data/NDVI/NDVI_na.tif", format="GTiff", overwrite=T)
+### step 1: 1m Canopy presence/absence map -- set no canopy (0) values to NA
+# bos.can <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m.tif")
+# bos.can.dat <- as.data.table(as.data.frame(bos.can))
+# bos.can.dat[bostoncanopy_1m==0,] <- NA
+# bos.can.na <- raster(bos.can)
+# bos.can.na <- setValues(bos.can.na, bos.can.dat$bostoncanopy_1m)
+# writeRaster(bos.can.na, "E:/FragEVI/data/dataverse_files/bostoncanopy_1m_na.tif", format="GTiff", overwrite=T, datatype="INT1U")
+bos.can.na <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m_na.tif")
 
-### new NDVI resampled in arc
-ndvi.res <- raster("E:/FragEVI/data/NDVI/NDVI_1m_res_cangrid.tif")
-# ndvi.res <- raster("/Volumes/Ultra/Ultra2/Users/atrlica/FragEVI/boston/NDVI_1m_res_cangrid.tif")
 
-## 1m Canopy presence/absence map
-bos.can <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m.tif")
-# repl.na <- function(x){
-#   x[x==0] <- NA
-#   return(x)
-# }
-# rasterOptions(chunksize = 2e+6)
-# object.size(bos.can)/1048600 ### object size in MB
-bos.can.dat <- as.data.table(as.data.frame(bos.can))
-bos.can.dat[bostoncanopy_1m==0,] <- NA
-bos.can.na <- raster(bos.can)
-bos.can.na <- setValues(bos.can.na, bos.can.dat$bostoncanopy_1m)
-# bos.can.na <- repl.na(bos.can)
-writeRaster(bos.can.na, "E:/FragEVI/data/dataverse_files/bostoncanopy_1m_na.tif", format="GTiff", overwrite=T, datatype="INT1U")
-# bos.can <- raster("/Volumes/Ultra/Ultra2/Users/atrlica/FragEVI/boston/bostoncanopy_1m.tif")
+### step 2: read in 2.4m Quickbird NDVI map, filter for NA
+bos.ndvi <- raster("E:/FragEVI/data/NDVI/NDVI.img") ## original Quickbird NDVI, manually process for 0==NA
+bos.ndvi.dat <- as.data.table(as.data.frame(bos.ndvi))
+bos.ndvi.dat[NDVI==0, NDVI:=NA]
+bos.ndvi.na <- setValues(bos.ndvi, values = bos.ndvi.dat[,NDVI]) ## good, full map, UTM 19N
+writeRaster(bos.ndvi.na, filename="E:/FragEVI/data/NDVI/NDVI_na.tif", format="GTiff", overwrite=T)
 
-### test polygons imported from Arc (approx 100x100m)
-t1 <- readOGR(dsn="E:/FragEVI/data/AOI", layer = "test.sm1")
-t1 <- spTransform(t1, crs(bos.can))
-t2 <- readOGR(dsn="E:/FragEVI/data/AOI", layer = "test.sm2")
-t2 <- spTransform(t2, crs(bos.can))
-t3 <- readOGR(dsn="E:/FragEVI/data/AOI", layer = "test.sm3")
-t3 <- spTransform(t3, crs(bos.can))
-t4 <- readOGR(dsn="E:/FragEVI/data/AOI", layer = "test.sm4")
-t4 <- spTransform(t4, crs(bos.can))
+### step 3: put NDVI app through arc and resample+snap to grid for 1m canopy map - be sure the end product is NAD83 UTM19N
+### NEED PYTHON IMBED HERE
 
-par(mfrow=c(1,2))
+### step 4: land cover classification for Boston using the 1m resampled NDVI (0 = barren, 1 = grass, 2 = canopy)
+bos.ndvi <- raster("E:/FragEVI/data/NDVI/NDVI_1m_res_cangrid.tif")
+bos.ndvi <- crop(bos.ndvi, bos.can.na)
 
-### land cover classification using the good NDVI (0 = barren, 1 = grass, 2 = canopy)
-ndvi.cr <- crop(ndvi.res, bos.can)
-bos <- stack(bos.can, ndvi.cr)
-# bos.dat <- as.data.table(as.data.frame(bos)) ## Poland cannot into space, too large to do all at once
-
-### test 1, good NDVI 1m, SW corner of Public Gardens -- seems to find grassy/barren areas between trees alright, sees impervious as barren
-can.t1 <- crop(bos.can, t1)
-can.t1[can.t1==0] <- NA
-writeRaster(can.t1, filename="E:/FragEVI/processed/can.t1.tif", overwrite=T, datatype="INT1U")
-ndvi.t1 <- crop(ndvi.cr, t1)
-plot(can.t1); plot(t1, add=T)
-plot(ndvi.t1); plot(t1, add=T)
-t1.dat <- as.data.table(as.data.frame(stack(can.t1, ndvi.t1)))
-names(t1.dat) <- c("can", "ndvi")
-t1.dat[ndvi<0.2, cov:=0]
-t1.dat[can==0 & ndvi>0.2, cov:=1]
-t1.dat[can==1, cov:=2]
-cov.t1 <- setValues(can.t1, t1.dat[,cov])
-plot(can.t1); plot(t1, add=T)
-plot(cov.t1); plot(t1, add=T)
-
-### test 2, good NDVI 1m, Alston Suburb --some marginal confusion near canopies but also sees what look like residential lawns
-can.t2 <- crop(bos.can, t2)
-writeRaster(can.t2, filename="E:/FragEVI/processed/can.t2.tif", format="GTiff", overwrite=T)
-ndvi.t2 <- crop(ndvi.cr, t2)
-plot(can.t2); plot(t2, add=T)
-plot(ndvi.t2); plot(t2, add=T)
-t2.dat <- as.data.table(as.data.frame(stack(can.t2, ndvi.t2)))
-names(t2.dat) <- c("can", "ndvi")
-t2.dat[ndvi<0.2, cov:=0]
-t2.dat[can==0 & ndvi>0.2, cov:=1]
-t2.dat[can==1, cov:=2]
-cov.t2 <- setValues(can.t2, t2.dat[,cov])
-plot(can.t2); plot(t2, add=T)
-plot(cov.t2); plot(t2, add=T)
-
-### test 3, good NDVI 1m, forest edge with ball park, Franklin Park S of track -- detects clear edge with lawn/baseball field, even detects what look like barren places at park periphery
-can.t3 <- crop(bos.can, t3)
-writeRaster(can.t3, filename="E:/FragEVI/processed/can.t3.tif", format="GTiff", overwrite=T)
-ndvi.t3 <- crop(ndvi.cr, t3)
-plot(can.t3); plot(t3, add=T)
-plot(ndvi.t3); plot(t3, add=T)
-t3.dat <- as.data.table(as.data.frame(stack(can.t3, ndvi.t3)))
-names(t3.dat) <- c("can", "ndvi")
-t3.dat[ndvi<0.2, cov:=0]
-t3.dat[can==0 & ndvi>0.2, cov:=1]
-t3.dat[can==1, cov:=2]
-cov.t3 <- setValues(can.t3, t3.dat[,cov])
-plot(can.t3); plot(t3, add=T)
-plot(cov.t3); plot(t3, add=T)
-
-### test 4, good NDVI 1m, forest patch in Stonybrook Park S of Turtle Pond -- sees canopy except for a few gaps with grass
-ndvi.t4 <- crop(ndvi.cr, t4)
-writeRaster(can.t4, filename="E:/FragEVI/processed/can.t4.tif", format="GTiff", overwrite=T)
-plot(can.t4); plot(t4, add=T)
-plot(ndvi.t4); plot(t4, add=T)
-t4.dat <- as.data.table(as.data.frame(stack(can.t4, ndvi.t4)))
-names(t4.dat) <- c("can", "ndvi")
-t4.dat[ndvi<0.2, cov:=0]
-t4.dat[can==0 & ndvi>=0.2, cov:=1]
-t4.dat[can==1, cov:=2]
-cov.t4 <- setValues(can.t4, t4.dat[,cov])
-plot(can.t4); plot(t4, add=T)
-plot(cov.t4); plot(t4, add=T) ## color scale goofballed -- note no barren present
-
-### use 1m canopy and NDVI to reclassify map into barren/grass/tree (0,1,2)
 ### function to process serially in chunks #### THIS WORKS BEAUTIFULLY, TAKES ~15 MIN FOR ALL OF BOSTON
-cover.bl <- function(x, y, filename) {
+cover.bl <- function(x, y, filename) { # x is canopy, y is ndvi
   out <- raster(x)
   bs <- blockSize(out)
   out <- writeStart(out, filename, overwrite=TRUE, format="GTiff")
@@ -178,10 +101,9 @@ cover.bl <- function(x, y, filename) {
   out <- writeStop(out)
   return(out)
 }
-s <- cover.bl(bos.can, ndvi.cr, filename="processed/bos.cov.tif")
+s <- cover.bl(bos.can.na, bos.ndvi, filename="processed/bos.cov.tif")
 plot(s)
 
-# 
 # ### function for row-by-row replacement of raster values (this works but is slower)
 # cover.f <- function(x, y, filename) { #x is canopy, y is ndvi
 #   out <- raster(x)
@@ -199,6 +121,19 @@ plot(s)
 #   return(out)
 # }
 # s <- cover.f(bos.can, ndvi.cr, filename="E:/FragEVI/processed/bos.cov.test.tif")
+# plot(s)
+
+
+#### use canopy map to get buffer zones (10 20 30 m)
+bos.can <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m.tif")
+ 
+# call python script that handles scrubbing the canopy gaps for tiny gaps, then exports the 10 20 30 m buffers
+pyth.path = './Rscripts/canopy_process.py'
+output = system2('python.exe', args=pyth.path, stdout=TRUE)
+print(paste("ArcPy working on canopy gaps: ", output))
+
+
+
 
 ### Test Code: Process NAIP 1m CIR data to NDVI
 ### NOTE SO MUCH: BAND ORDER IN NAIP CIR DATA IS RED GREEN BLUE NIR (1,2,3,4)
@@ -209,9 +144,3 @@ naip.dat[, ndvi:=((band4-band1)/(band4+band1))]
 ndvi.r <- raster(naip.test[[1]])
 ndvi.r <- setValues(x = ndvi.r, values=naip.dat[,ndvi])
 writeRaster(ndvi.r, filename="processed/NAIP.ndvi.test.tif", format="GTiff", overwrite=T)
-
-
-### get edge classification for canopies
-Sys.which("gdal_polygonize.py")
-
-
