ddf4f643675ea8be87cd45d1220377c8916e992f
Andrew Trlica <atrlica@scc-pi1.scc.bu.edu>
2018-02-11 20:18:16 -0500

    1m Boston stack of edges, cover, isa, canopy, NDVI is prepared, arcpy steps inline.
    Need to whip the 30m collation steps into shape.

--- Rscripts/frag.processing.R
+++ Rscripts/frag.processing.R
@@ -4,6 +4,7 @@ library(rgdal)
 library(rgeos)
 library(raster)
 library(data.table)
+library(rPython)
 
 
 #########
@@ -12,7 +13,8 @@ library(data.table)
 ### use Arcmap to resample + snap to 1m grid (bilinear) for NDVI 2.4m -- get good alignment with original data and features in canopy map
 
 ### step 1: 1m Canopy presence/absence map 
-bos.can <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m.tif")
+bos.can <- raster("data/dataverse_files/bostoncanopy_1m.tif")
+
 ### set no canopy (0) values to NA
 # bos.can.dat <- as.data.table(as.data.frame(bos.can))
 # bos.can.dat[bostoncanopy_1m==0,] <- NA
@@ -21,17 +23,17 @@ bos.can <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m.tif")
 # writeRaster(bos.can.na, "E:/FragEVI/data/dataverse_files/bostoncanopy_1m_na.tif", format="GTiff", overwrite=T, datatype="INT1U")
 # bos.can.na <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m_na.tif")
 
-### step 3: put NDVI.img through Arc and resample+snap to grid for 1m canopy map - be sure the end product is NAD83 UTM19N
+### step 3 (must be performed on desktop): put NDVI.img through Arc and resample+snap to grid for 1m canopy map - be sure the end product is NAD83 UTM19N
 pyth.path = './Rscripts/NDVI_resamp.py'
 output = system2('C:/Python27/ArcGIS10.4/python.exe', args=pyth.path, stdout=TRUE)
 print(paste("ArcPy working on NDVI resample: ", output))
 
 ### step 4: land cover classification for Boston using the 1m resampled NDVI (0 = barren, 1 = grass, 2 = canopy)
-bos.ndvi <- raster("E:/FragEVI/data/NDVI/NDVI_1m_res_cangrid.tif")
-bos.can.na <- raster("E:/FragEVI/data/dataverse_files/bostoncanopy_1m_na.tif")
+bos.ndvi <- raster("data/NDVI/NDVI_1m_res_cangrid.tif")
+bos.can.na <- raster("data/dataverse_files/bostoncanopy_1m_na.tif")
 bos.ndvi <- crop(bos.ndvi, bos.can.na)
 cover.bl <- function(x, y, filename) { # x is canopy, y is ndvi
-  if(file.exists("E:/FragEVI/processed/bos.cov.tif")){file.remove("E:/FragEVI/processed/bos.cov.tif")}
+  if(file.exists("processed/bos.cov.tif")){file.remove("processed/bos.cov.tif")}
   out <- raster(x)
   bs <- blockSize(out)
   out <- writeStart(out, filename, overwrite=TRUE, format="GTiff")
@@ -47,12 +49,10 @@ cover.bl <- function(x, y, filename) { # x is canopy, y is ndvi
   out <- writeStop(out)
   return(out)
 }
-bos.cov <- cover.bl(bos.can, bos.ndvi, filename="E:/FragEVI/processed/bos.cov.tif")
-
+bos.cov <- cover.bl(bos.can, bos.ndvi, filename="processed/bos.cov.tif")
 
 ### Processing for 1m canopy map for edge class
-
-### call python script for identifying canopy edge distance
+### call python script for identifying canopy edge distance (desktop only)
 pyth.path = './Rscripts/canopy_process.py'
 output = system2('C:/Python27/ArcGIS10.4/python.exe', args=pyth.path, stdout=TRUE)
 print(output)
@@ -65,7 +65,7 @@ ed3 <- raster("processed/nocan_30mbuff.tif")
 ed1 <- extend(ed1, bos.cov) # edge and cover on same grid already
 ed2 <- extend(ed2, bos.cov)
 ed3 <- extend(ed3, bos.cov)
-edges <- stack(ed1, ed2, ed3, bos.cov) # just to make damn sure everything lines up
+# edges <- stack(ed1, ed2, ed3, bos.cov) # just to make damn sure everything lines up
 
 edges.bl <- function(x, y, filename) { # x is edge class, y is cover class
   out <- raster(x)
@@ -89,14 +89,31 @@ t <- edges.bl(ed2, bos.cov, filename="processed/edge20m.tif")
 u <- edges.bl(ed3, bos.cov, filename="processed/edge30m.tif")
 
 ### package up all the 1m data for Boston and write to disk
-bos.stack <- stack(bos.can.na, bos.ndvi, bos.cov, s, t, u)
-writeRaster()
+ed10 <- raster("processed/edge10m.tif")
+ed20 <- raster("processed/edge20m.tif")
+ed30 <- raster("processed/edge30m.tif")
+bos.cov <- raster("processed/bos.cov.tif")
+bos.can <- raster("data/dataverse_files/bostoncanopy_1m.tif")
+bos.ndvi <- raster("data/NDVI/NDVI_1m_res_cangrid.tif")
+bos.ndvi <- crop(bos.ndvi, bos.can)
 
-### aggregate 
-###### get aggregated areas for 1m data at 30 m grid
+# ### add in 1m ISA (go get it, crop it, then reproject)
+# isa <- raster("/projectnb/buultra/atrlica/BosAlbedo/data/ISA/isa_1m_AOI.tif")
+# towns <- readOGR(dsn = "/projectnb/buultra/atrlica/BosAlbedo/data/towns/town_AOI.shp", layer = "town_AOI" )
+# bos.bord <- towns[towns@data$TOWN=="BOSTON",]
+# bos.bord <- spTransform(bos.bord, crs(isa))
+# bos.isa <- crop(isa, bos.bord)
+# writeRaster(bos.isa, filename="processed/bos_isa_1m.tif", format="GTiff", overwrite=T)
+# # python.load("Rscripts/bosISA_resamp.py") ### test this
+# bos.isa <- raster("processed/isa_cangrid.tif")
+# bos.isa <- crop(bos.isa, bos.can)
+
+bos.stack <- stack(bos.can, bos.ndvi, bos.cov, bos.isa, ed10, ed20, ed30)
+writeRaster(bos.stack, "processed/bos.stack.1m.tif", format="GTiff", overwrite=T)
+### do I add a 1m raster version of the LULC layer here? at all useful past the bos.cov information?
+
 #######
-### the following chunk aggregates 1m ISA to 30m landsat grid
-### intermediate steps run on the cluster
+### 30 m aggregate of 
 ### read in AOI for crs description
 AOI <- readOGR(dsn="/projectnb/buultra/atrlica/BosAlbedo/data/AOI/AOI_simple_NAD83UTM19N/", layer="AOI_simple_NAD83UTM19N")
 master.crs <- crs(AOI)
@@ -119,10 +136,11 @@ isa <- raster("/projectnb/buultra/atrlica/BosAlbedo/data/ISA/isa_1m_AOI.tif")
 # }
 # isa.na <- calc(isa, fun)
 # isa.na.agg <- aggregate(isa.na, fact=30, fun=mean, na.rm=FALSE) # get mean ISA per 30 m footprint
-# writeRaster(isa.na.agg, filename="processed/isa.30m.tif", format="GTiff", overwrite=T)
+# isa.na.agg <- projectRaster(isa.na.agg, crs=master.crs)
+# writeRaster(isa.na.agg, filename="processed/isa.30m.NAD83.tif", format="GTiff", overwrite=T)
 
 ### read in 30 m ISA, align to the EVI grid and stack
-isa.na.agg <- raster("processed/isa.30m.tif")
+isa.na.agg <- raster("processed/isa.30m.NAD83.tif")
 evi.r <- raster("E:/FragEVI/processed/EVI/030005-6_2010-2012_EVI_NAD83.tif") ## this is the July 2010-2012 AOI EVI composite
 isa.pr <- projectRaster(isa.na.agg, evi.r, method="ngb", filename="processed/isa.30m.NAD83.tif", format="GTiff", overwrite=T)
 
